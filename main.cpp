e86ff9d10da4db63c00dc97b6c24f933f5e08fcd
Xmde <erve2005@gmail.com>
2020-11-03 20:34:50 -0800

    Added Comments

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+//Function to convert a String into all Uppercase
 string toUpperCase(string str){
     string output;
     for(int i = 0; i < str.length(); i++){
@@ -14,6 +15,7 @@ string toUpperCase(string str){
     return output;
 }
 
+//Function to Convert the card number into the cards face
 string toCards(int card){
     switch(card) {
         case 1:
@@ -45,6 +47,7 @@ string toCards(int card){
     }
 }
 
+//funtion to see if a player has busted.
 bool checkBust(vector<int> hand){
     int total = 0;
     for(int i = 0; i < hand.size(); i++){
@@ -56,6 +59,7 @@ bool checkBust(vector<int> hand){
     return false;
 }
 
+//function that returns the total value of the players hand.
 int totalCards(vector<int> hand){
     int total = 0;
     for(int i = 0; i < hand.size(); i++){
@@ -64,6 +68,7 @@ int totalCards(vector<int> hand){
     return total;
 }
 
+//Function to see if the dealer wins. It takes in two players hands. A Player and a dealer.
 bool dealerWins(vector<int> playerHand, vector<int> dealerHand){
     int playerTotal = 0;
     int dealerTotal = 0;
@@ -76,6 +81,7 @@ bool dealerWins(vector<int> playerHand, vector<int> dealerHand){
     return dealerTotal >= playerTotal;
 }
 
+//Function to see if the dealer should hit to stay.
 bool dealerHits(vector<int> dealerHand){
     int total = 0;
     for(int i = 0; i < dealerHand.size(); i++){
@@ -90,6 +96,7 @@ bool dealerHits(vector<int> dealerHand){
 int main() {
     string usr;
     int numOfPlayers;
+    //All the players including the dealer are stored in a 2d vector.
     vector<vector<int>> playersHands;
 
     //Starting script asking If you  want to play a game and exiting if you say No and continuing if you say Yes.
@@ -129,48 +136,61 @@ int main() {
     }
 
     int newCard;
-
+    //Prints one of the dealers cards.
     cout << "\nThe dealers First Card is " + toCards(playersHands[0][0]);
 
+    //This is the main loop for the players to see if they want to hit or skip.
     for(int i = 1; i <= numOfPlayers; i++){
         cout << "\n\nPlayer " + to_string(i) + ":Your cards are " + toCards(playersHands[i][0]) + " and " + toCards(playersHands[i][1]) + ". TOTAL: " + to_string(totalCards(playersHands[i]));
         hitskip:
         cout << "\nDo you want to Hit or Skip (H/S)";
         cin >> usr;
         if(toUpperCase(usr) == "H"){
+            //Generates a New card if the player decides to hit
             newCard = rand()%13+1;
+            //Adds that card to that players hand inside of the 2d vector.
             playersHands[i].push_back(newCard);
+            //Displays info to the player about the card recived and their new total.
             cout << "You got a " + toCards(newCard) + ". TOTAL: " + to_string(totalCards(playersHands[i]));
+            //checks to see if the player has busted and if they havn't goes back to up see if the player wants to hit or skip.
             if(!checkBust(playersHands[i])){
                 goto hitskip;
             }
-            playersHands[i][0] = -100;
+            //If the player busts it sets their first card to -500 making further comparisions show the dealer as a winner.
+            playersHands[i][0] = -500;
             cout << "\nYou BUST. Better luck next time.";
         }else if (toUpperCase(usr) == "S"){
         }else{
+            //Fail safe to make sure that player either hits or skips.
             cout << "\nPlease enter either a H or S";
             goto hitskip;
         }
     }
 
+   //Dealers Turn and all the main Logic for him.
     cout << "\n\nDealers Turn";
     cout << "\nDealers Cards: " + toCards(playersHands[0][0]) + " and " + toCards(playersHands[0][1]) + ". TOTAL: " + to_string(totalCards(playersHands[0]));
+    //Loop itterating as long as the dealer doesn't bust.
     while (!checkBust(playersHands[0])){
+        //If the dealer should hit it gives him a new card.
         if(dealerHits(playersHands[0])){
             newCard = rand()%13+1;
             playersHands[0].push_back(newCard);
             cout << "\nDealer Hits and Recives a " + toCards(newCard) + ". TOTAL: " + to_string(totalCards(playersHands[0]));
         }else{
+            //If the dealer shouldn't hit it breaks out of the loop.
             goto dealerNoBust;
         }
     }
+    //Print statement for when the dealer busts. Also sets the dealers first card to -100 to show that the dealer has lost. This value is larger than the -500 for the player so if both parties bust the dealer still wins.
     cout << "\nDealer Busts!";
-    playersHands[0][0] = -50;
+    playersHands[0][0] = -100;
 
     dealerNoBust:
 
     cout << "\n";
 
+    //Prints out all the final standings on who has won and who has lost.
     for(int i = 1; i <= numOfPlayers; i++){
         if(dealerWins(playersHands[i], playersHands[0])){
             cout << "\nPlayer " + to_string(i) + ": Better luck Next time Looks like the dealer wins.";
